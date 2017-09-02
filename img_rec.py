c7af1fd3657395bcbc7a05bdb40f8ac5e72848a4
Eric <aokaces@gmail.com>
2017-09-01 22:29:55 -0400

    couple of fixes

--- img_rec.py
+++ img_rec.py
@@ -3,16 +3,19 @@
     Note: We use numpy instead of pandas as there is no need for labels
          Since pixels will be unrolled, all we need is a matrix functionality
          Therefore, numpy will allow a ML system to learn faster than in pandas
+
+    Python 3.7
+    OpenCV not supported in this version of python
 """
 
 import numpy as np
 import scipy.misc as sp
 import numpy.linalg as alg
 import os
-
+import sys
+#Clearing interpreter
 os.system('cls')
-final_matrix = np.empty()
-
+#Function definitions
 def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
@@ -31,16 +34,34 @@ def img_process(path, image_name):
     file_type = image_name.split('.')
     return None if file_type[-1].lower() not in formats else sp.imread(path + '\\' + image_name, flatten = True)
 
+#ML implementation starts here
 try:
     img_path = input("Folder Path: ")
+    files = os.listdir(img_path)
 except:
-    print("Path not found")
+    print("Path not found.")
+    print("Exiting.")
+    sys.exit()
 
-#def unroll(img_array):
-#    """Function will unroll a numpy array into a single vector
-#        and append to another numpy array
-#    """
-#    temp = img_array.ravel()
+#ANN dimensions
+temp = img_process(img_path, files[1])
+input_layer_size = len(temp.ravel())            #I know, it's a shitty fix
+hidden_layer_size = 3
 
+del temp                                   #Clear memory
+final_matrix = np.empty([input_layer_size, len(files)])
 
-print(sigmoid(0))
+i = 0                                      #So we can change each row in the matrix
+
+for img in files:
+    i = i + 1
+    file_ = img_process(img_path, img)
+    if file_ is None:
+        continue
+
+    file_ = file_.ravel()
+    np.vstack([final_matrix, file_]) #Need to figure out a way to append to matrix
+    """Need to test this loop"""
+
+#need to unroll each image and append one by one to a single numpy array
+print(final_matrix)
