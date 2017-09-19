08fcff97e34bd0bb543a267eae24e5458b3060ca
Eric <aokaces@gmail.com>
2017-09-19 13:31:46 -0400

    Cost function completed, Note: still needs grad and back prop

--- MNIST/CostFunction.py
+++ MNIST/CostFunction.py
@@ -1,6 +1,13 @@
 """MNIST Cost Function"""
+import numpy as np
+
+def sigmoid(z):
+    z = np.clip(z, -300, 300)           #prevents overflow error
+    return 1/(1 + np.exp(-1.0 * z))
 
 def cost_func(set_matrix, y, lamb, theta1, theta2):
+    """1 layer neural network"""
+    m = len(set_matrix)
 
     Z_2 = np.dot(set_matrix, theta1.T)
     A_2 = sigmoid(Z_2)
@@ -9,7 +16,9 @@ def cost_func(set_matrix, y, lamb, theta1, theta2):
     Z_3 = np.dot(A_2, theta2.T)
     prediction = sigmoid(Z_3)
 
-    J = (1/m) * sum(sum((-y * log(prediction)) - ((1 - y) * log(1 - prediction))))
+    err = (-y * np.log(prediction)) - ((1 - y) * np.log(1 - prediction))
+    J = (float(1)/ m) * err.sum()
+
     t1_fix = np.delete(theta1, 0, axis = 1)
     t2_fix = np.delete(theta2, 0, axis = 1)
-    return J + (lamb / (2 * m)) * (sum(sum(np.square(t1_fix))) + sum(sum(np.square(t2_fix))))
+    return J + ((lamb / (2 * m)) * (t1_fix.sum() + t2_fix.sum()))
